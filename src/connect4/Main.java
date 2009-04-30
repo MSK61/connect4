604e101cb8ce924ba53a0fb5a70f12cb1b3c1681
Mohammed El-Afifi <Mohammed_ElAfifi@yahoo.com>
2009-04-30 08:59:10 +0000

    initial check-in

---
+++ src/connect4/Main.java
@@ -0,0 +1,48 @@
+/************************************************************
+
+    Copyright 2009 Mohammed El-Afifi
+    This file is part of Connect4.
+
+    Connect4 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Connect4 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Connect4.  If not, see <http://www.gnu.org/licenses/>.
+
+file:         Main.java
+
+function:     application entry point
+
+description:  runs a connect4 game between the computer and the user
+
+author:       Mohammed El-Afifi (ME)
+
+environment:  NetBeans IDE 6.5, Fedora release 10 (Cambridge)
+
+notes:        This is a private program.
+
+************************************************************/
+
+package connect4;
+
+/**
+ *
+ * @author MAfifi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
